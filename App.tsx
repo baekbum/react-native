e1448f15e8ec151926a74d33d6110bbe84c7f3b4
baekbum <baekbum828@naver.com>
2021-07-06 18:31:10 +0900

    초기 커밋

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Platform, Dimensions } from 'react-native';
+import { Colors } from 'react-native-paper';
+import Color from 'color';
 
 export default function App() {
+  const {height, width} = Dimensions.get('window');
+  console.log(Platform.OS);
+  console.log(`height: ${height}, width: ${width}`);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.SafeAreaView}>
+      <Text style={styles.Text}>Hello~! react-native</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  SafeAreaView: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.blue500,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  Text: {
+    fontSize: 20,
+    color: Color(Colors.blue500).alpha(0.7).lighten(0.9).toString()
+  }
 });
